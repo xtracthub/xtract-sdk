d5ac0510d4ea88e33074889cc8c84dfd174daf87
tskluzac <skluzacek@uchicago.edu>
2020-06-11 13:33:49 -0500

    First pass at figshare.

--- xtract_sdk/downloaders/base.py
+++ xtract_sdk/downloaders/base.py
@@ -1,2 +1,33 @@
 
-# TODO: Once I have a better feel for what can be abstracted from all other functions.
+from abc import ABCMeta, abstractmethod
+import tempfile
+import os
+
+
+class DownloaderInterface(metaclass=ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'fetch') and
+                callable(subclass.fetch) and
+                hasattr(subclass, 'batch_fetch') and
+                callable(subclass.batch_fetch) or NotImplemented)
+
+    def __init__(self, download_type="file", mkdir=False):
+        self.download_type = download_type
+        self.orig_dir = os.getcwd()
+
+        if mkdir:
+            self.new_dir = tempfile.mkdtemp()
+        else:
+            self.new_dir = self.orig_dir
+
+        self.success_files = set()
+        self.fail_files = set()
+
+    @abstractmethod
+    def fetch(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def batch_fetch(self):
+        raise NotImplementedError
---
+++ xtract_sdk/downloaders/figshare.py
@@ -0,0 +1,47 @@
+
+import requests
+import os
+
+from xtract_sdk.downloaders.base import DownloaderInterface
+
+
+class FigshareDownloader(DownloaderInterface):
+
+    def __init__(self, download_type="file", mkdir=False):
+        super().__init__(download_type, mkdir)
+
+    def fetch(self, fid, path):
+
+        """Download a file from figshare
+
+        Stolen (with credit) from Logan Ward (@wardlt)
+
+        Args:
+            fid (int): ID number of figshare article
+            path (str): Download path
+        """
+
+        # Get the article details
+        art_details = requests.get('https://api.figshare.com/v2/articles/{}/files'.format(fid)).json()
+
+        # Loop over each file
+        for detail in art_details:
+            # Make the download path
+            filename = detail['name']
+            data_path = os.path.join(path, filename)
+
+            # Download the file
+            req = requests.get(art_details[0]['download_url'], stream=True)
+            print(data_path)
+            with open(data_path, 'wb') as fp:
+                for chunk in req.iter_content(chunk_size=1024 ** 2):
+                    fp.write(chunk)
+
+    def batch_fetch(self):
+        raise NotImplementedError("Batch fetching not yet implemented for FigShare")
+
+
+fs = FigshareDownloader("file", False)
+fid = 1057646
+path = '/Users/tylerskluzacek/Desktop/'
+fs.fetch(fid, path)
