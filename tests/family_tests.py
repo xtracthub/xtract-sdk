1f61b5956b16cf6d5b024b88454f40f49f49490a
tskluzac <skluzacek@uchicago.edu>
2020-06-25 12:44:55 -0500

    Adding family/group packager tests.

---
+++ tests/family_tests.py
@@ -0,0 +1,11 @@
+
+from xtract_sdk.packagers.family import Family
+
+fam = Family(str(0), headers={'potato': 'tomato'}, metadata=None)
+
+group_id = fam.add_group(files=['a', 'b', 'c'], parser="camel")
+group_id2 = fam.add_group(files=['b', 'c', 'd', 'e'], parser="potato")
+print(f"Added group with ID: {group_id}")
+
+assert type(group_id) is str, "fam.add_group is not returning an id of type str"
+assert fam.files == {'a', 'b', 'c', 'd', 'e'}, "fam.files not properly inheriting group.files"
