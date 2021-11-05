ef7fd1a5647dd94a79d05f433929522c91ca7fc7
tylergroshong <tylergroshong@uchicago.edu>
2021-11-04 23:07:12 -0500

    worked on .crawl() and .get_crawl_status()

--- xtract_sdk/client/xtract_client.py
+++ xtract_sdk/client/xtract_client.py
@@ -1,10 +1,20 @@
 
+import time
 import json
 import requests
 import mdf_toolbox
-from xtract_sdk.client.routes import XTRACT_CRAWLER, XTRACT_CRAWLER_DEV, XTRACT_SERVICE, XTRACT_SERVICE_DEV
+from routes import XTRACT_CRAWLER, XTRACT_CRAWLER_DEV, XTRACT_SERVICE, XTRACT_SERVICE_DEV
 
-#test
+class XtractEndpoint:
+
+    def __init__(self, repo_type, globus_ep_id, dirs, grouper, funcx_ep_id=None):
+
+        self.repo_type = repo_type
+        self.globus_ep_id = globus_ep_id
+        self.funcx_ep_id = funcx_ep_id
+        self.dirs = dirs
+        self.grouper = grouper
+
 class XtractClient:
 
     def __init__(self, auth_scopes=None, dev=False):
@@ -16,6 +26,7 @@ class XtractClient:
             self.crawl_url = XTRACT_CRAWLER
             self.extract_url = XTRACT_SERVICE
 
+        self.funcx_scope = "https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all"
         scopes = [
             "openid",
             "data_mdf",
@@ -23,7 +34,7 @@ class XtractClient:
             "petrel",
             "transfer",
             "dlhub",
-            "https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all",
+            self.funcx_scope
         ]
 
         if auth_scopes is not None:
@@ -37,42 +48,58 @@ class XtractClient:
             no_browser=False,
             no_local_server=False)
 
-    def crawl(self, repo_type, **kwargs):
+        self.crawl_ids = None
+
+    def crawl(self, xeps, **kwargs):
+
+        crawl_ids = []
+
+        self.base_url = f'{self.crawl_url}crawl'
+        print(f"Crawl URL is : {self.base_url}")
 
         # TODO: GET THE CRAWL WORKING!
 
-        if repo_type == "GLOBUS":
-            crawl_params = ["eid", "dir_path", "grouper", "https_info"]
-            payload = {"repo_type": repo_type}
+        for xep in xeps:
+            ep_dicts = []
+            if xep.repo_type.lower() == "globus":
+                ep_dicts.append({'repo_type': xep.repo_type,
+                                 'eid': xep.globus_ep_id,
+                                 'dir_paths': xep.dirs,
+                                 'grouper': xep.grouper})
+            #crawl_params = ["eid", "dir_path", "grouper", "https_info"]
+            #payload = {"repo_type": repo_type}
+            #for param in crawl_params:
+            #    try:
+            #        payload[param] = kwargs.get(param)
+            #    except KeyError:
+            #        raise Exception(f"Missing parameter {param} for {repo_type} crawl")
 
-            for param in crawl_params:
-                try:
-                    payload[param] = kwargs.get(param)
-                except KeyError:
-                    raise Exception(f"Missing parameter {param} for {repo_type} crawl")
+                crawl_tokens = {'Transfer': self.auths['transfer'].authorizer.access_token,
+                                'Authorization': f"Bearer {self.auths['transfer'].authorizer.access_token}",
+                                'FuncX': self.auths[self.funcx_scope].access_token}
 
-            payload["Transfer"] = self.transfer_token
-            payload["Authorization"] = self.funcx_token
+                crawl_req = requests.post(self.base_url, json={'endpoints': ep_dicts,
+                                                                'tokens': crawl_tokens})
 
-            crawl_req = requests.post(self.crawl_url, json=payload)
+            # elif repo_type == "GDRIVE":
+            #     payload = {"auth_creds": self.gdrive_auth_creds, "repo_type": repo_type}
+            #     crawl_req = requests.post(self.crawl_url, data=pickle.dumps(payload))
+            else:
+                raise Exception(f"repo_type {xep.repo_type} is invalid")
 
-        # elif repo_type == "GDRIVE":
-        #     payload = {"auth_creds": self.gdrive_auth_creds, "repo_type": repo_type}
-        #     crawl_req = requests.post(self.crawl_url, data=pickle.dumps(payload))
-        else:
-            raise Exception(f"repo_type {repo_type} is invalid")
+            try:
+                crawl_id = json.loads(crawl_req.content)["crawl_id"]
+                print(f"Crawl ID: {crawl_id}")
+            except:  # TODO: Add a better catch based on status codes.
+                raise Exception(f"Crawl request failed with status {crawl_req.status_code}")
 
-        try:
-            crawl_id = json.loads(crawl_req.content)["crawl_id"]
-        except:  # TODO: Add a better catch based on status codes.
-            raise Exception(f"Crawl request failed with status {crawl_req.status_code}")
+            crawl_ids.append(crawl_id)
+            #self.repo_type = repo_type
 
-        self.crawl_id = crawl_id
-        self.repo_type = repo_type
+        self.crawl_ids = crawl_ids
+        return crawl_ids
 
-        return crawl_id
-
-    def get_crawl_status(self):
+    def get_crawl_status(self, user_crawl_ids=None):
         """Retrieves the crawl status of a job. .crawl() method must be run first.
 
         Returns
@@ -80,20 +107,48 @@ class XtractClient:
         crawl_status_content: dict
             Status of crawl job.
         """
-        crawl_status_url = f"{self.base_url}/get_crawl_status"
 
-        if self.crawl_id is None:
+        if (self.crawl_ids is None) and (user_crawl_ids is not None):
+            self.crawl_ids = user_crawl_ids
+
+        crawl_status_url = f"{self.crawl_url}/get_crawl_status"
+
+        if self.crawl_ids is None:
             raise Exception("Missing crawl ID. A crawl ID must be provided or the .crawl() method must be run")
 
-        payload = {"crawl_id": self.crawl_id}
-        crawl_status_req = requests.get(crawl_status_url, json=payload)
+        statuses = []
 
-        try:
-            crawl_status_content = json.loads(crawl_status_req.content)
-        except:  # TODO: Add a better catch based on status codes. IDK what the default status code is.
-            raise Exception(f"Crawl status retrieval failed with status {crawl_status_req.status_code}")
+        for id in self.crawl_ids:
+            crawl_status = requests.get(crawl_status_url, json={'crawl_id': id})
+            try:
+                crawl_content = json.loads(crawl_status.content)
+            except:  # TODO: Add a better catch based on status codes. IDK what the default status code is.
+                raise Exception(f"Crawl status retrieval failed with status {crawl_status.status_code}")
+            statuses.append(crawl_content)
 
-        return crawl_status_content
+        return statuses
+
+xtr = XtractClient()
+xep1 = XtractEndpoint(repo_type="GLOBUS",
+                      globus_ep_id='4f99675c-ac1f-11ea-bee8-0e716405a293',
+                      dirs=['/cdiac/home/tskluzac/Downloads/'],
+                      grouper='file_is_group')
+xep2 = XtractEndpoint(repo_type="Globus",
+                      globus_ep_id='4f99675c-ac1f-11ea-bee8-0e716405a293',
+                      dirs=['/cdiac/home/tskluzac/Downloads/'],
+                      grouper='file_is_group')
+
+crawl_ids = xtr.crawl([xep1, xep2])
+
+while True:
+    statuses = xtr.get_crawl_status(crawl_ids)
+    for resp in statuses:
+        # if anything isn't done yet, we can't stop.
+        print(resp)
+        if resp['crawl_status'] != 'completed':
+            time.sleep(2)  # wait 2 seconds
+            continue
+
 
     # def extract(self, **kwargs):
     #     """Sends extract request to Xtract.
