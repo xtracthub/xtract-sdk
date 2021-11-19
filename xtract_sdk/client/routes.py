55203c190b1dd1befd448412edc8e612779d8051
tylergroshong <tylergroshong@uchicago.edu>
2021-11-19 14:18:21 -0600

    added extract url

--- xtract_sdk/client/routes.py
+++ xtract_sdk/client/routes.py
@@ -2,5 +2,5 @@
 XTRACT_CRAWLER = "http://xtractcrawler5-env.eba-akbhvznm.us-east-1.elasticbeanstalk.com/"
 XTRACT_CRAWLER_DEV = "TODO"
 
-XTRACT_SERVICE = "TODO"
+XTRACT_SERVICE = "http://xtractservice2-env.eba-xh7cjv4i.us-east-1.elasticbeanstalk.com/"
 XTRACT_SERVICE_DEV = "TODO"
