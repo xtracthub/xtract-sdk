a675d1446e27cac5fe79eb7284d6e5197607ad6f
tylergroshong <tylergroshong@uchicago.edu>
2021-11-19 00:43:18 -0600

    added local_mdata_path and remote_mdata_path as required arguments

--- xtract_sdk/endpoint/xtract_endpoint.py
+++ xtract_sdk/endpoint/xtract_endpoint.py
@@ -1,8 +1,10 @@
 class XtractEndpoint:
 
-    def __init__(self, repo_type, globus_ep_id, dirs, grouper, funcx_ep_id=None):
+    def __init__(self, repo_type, globus_ep_id, dirs, grouper, local_mdata_path, remote_mdata_path, funcx_ep_id=None):
         self.repo_type = repo_type
         self.globus_ep_id = globus_ep_id
         self.funcx_ep_id = funcx_ep_id
         self.dirs = dirs
         self.grouper = grouper
+        self.local_mdata_path = local_mdata_path
+        self.remote_mdata_path = remote_mdata_path
