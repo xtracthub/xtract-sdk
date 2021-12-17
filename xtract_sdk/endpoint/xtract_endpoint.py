08d2d39756845a479b35492900de6a4716cebe72
tskluzac <skluzacek@uchicago.edu>
2021-12-17 09:09:42 -0600

    adding container registration

--- xtract_sdk/endpoint/xtract_endpoint.py
+++ xtract_sdk/endpoint/xtract_endpoint.py
@@ -1,3 +1,10 @@
+
+import json
+import requests
+
+from xtract_sdk.client.routes import XTRACT_SERVICE, XTRACT_SERVICE_DEV
+
+
 class XtractEndpoint:
 
     def __init__(self, repo_type, globus_ep_id, dirs, grouper, local_mdata_path,
@@ -9,3 +16,10 @@ class XtractEndpoint:
         self.grouper = grouper
         self.local_mdata_path = local_mdata_path
         self.remote_mdata_path = remote_mdata_path
+
+    def register_containers(self, container_path):
+        """ Function to register containers with the central service. """
+        payload = {'fx_eid': self.funcx_ep_id,
+                   'container_path': container_path}
+        resp = requests.post(f"{XTRACT_SERVICE_DEV}config_containers", json=payload)
+        print(f"Register containers status: {json.loads(resp.content)['status']}")
