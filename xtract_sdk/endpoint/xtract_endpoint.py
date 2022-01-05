d023104ad2e2b531ce2434b76041248ee559e994
tylergroshong <tylergroshong@uchicago.edu>
2022-01-05 12:43:27 -0800

    raising error if globus endpoint ID or funcx endpoint ID are not strings (e.g. if they are given as lists, which will mess things up)

--- xtract_sdk/endpoint/xtract_endpoint.py
+++ xtract_sdk/endpoint/xtract_endpoint.py
@@ -3,8 +3,16 @@ class XtractEndpoint:
 
     def __init__(self, repo_type, globus_ep_id, dirs, grouper, local_mdata_path, funcx_ep_id=None):
         self.repo_type = repo_type
-        self.globus_ep_id = globus_ep_id
-        self.funcx_ep_id = funcx_ep_id
         self.dirs = dirs
         self.grouper = grouper
         self.local_mdata_path = local_mdata_path
+
+        if isinstance(globus_ep_id, str):
+            self.globus_ep_id = globus_ep_id
+        else:
+            raise Exception('Globus endpoint ID must be a string')
+
+        if isinstance(funcx_ep_id, str) or (not funcx_ep_id):
+            self.funcx_ep_id = funcx_ep_id
+        else:
+            raise Exception('FuncX endpoint ID must be a string')
